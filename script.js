36c3a782270a3c20d023071b3a1ce735c9625a71
vanessanutella <vaguochasam@gmail.com>
2023-04-25 12:32:33 -0400

    updating_project

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
+function initChart(chart) {
+    const ctx = document.querySelector('#myChart');
+  
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  
+    return new Chart(
+      chart,
+      config
+    )
+  }
+
 async function getData() {
-    const response = await fetch('https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json')
-    const data = await response.json()
+    //const storedData = localStorage.getItem("storedData");
+    //let parsedData = JSON.parse(storedData);
+    //const results = await fetch('https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json');
+    //const storedList = await results.json();
+    //localStorage.setItem("storedData", JSON.stringify(storedList));
+    //parsedData = storedList;
+    //console.log(storedList);
+    initChart(ctx);
 }
 
-let data = JSON.parse(this.response)
+/*
+function initChart(chart) {
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  const myChart = new Chart(
+    chart,
+    config
+  )
+} */
